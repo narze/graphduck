b63a4f5b0785b957468e0386c08aa4d50042fad5
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-23 18:04:03 +0700

    fix: fix resolving paths

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server'
 import { createConnection, getConnectionOptions } from 'typeorm'
-import { User } from './entities/user'
+import { User } from '@/entities/user'
+import { Book } from '@/entities/book'
 import { buildSchema } from 'type-graphql'
-import { HelloResolver } from './resolvers/hello'
-import { UserResolver } from './resolvers/user'
+import { HelloResolver } from '@/resolvers/hello'
+import { UserResolver } from '@/resolvers/user'
+import { BookResolver } from '@/resolvers/book'
 
 const main = async () => {
   const defaultConnectionOptions = await getConnectionOptions()
 
+  // TODO: auto include all entities & resolvers
   const conn = await createConnection({
     ...defaultConnectionOptions,
-    entities: [User],
+    entities: [User, Book],
   })
 
   await conn.synchronize()
 
   const schema = await buildSchema({
-    resolvers: [HelloResolver, UserResolver],
+    resolvers: [HelloResolver, UserResolver, BookResolver],
   })
 
   const server = new ApolloServer({ schema })
